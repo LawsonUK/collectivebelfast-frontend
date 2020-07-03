6c6721293e212c54700b61eb68a58853ee54ff88
LawsonUK <chrislawsonuk@hotmail.com>
2020-07-03 22:55:20 +0100

    styling for church page

--- src/pages/church.js
+++ src/pages/church.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Img from "gatsby-image"
+import ReactMarkdown from "react-markdown"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
@@ -9,6 +10,39 @@ import { graphql } from "gatsby"
 import "./church.scss"
 
 const IndexPage = ({ data }) => {
+  let count = 1
+
+  const renderHtml = data.allStrapiChurch.nodes[0].ministry.map(ministry => {
+    const html =
+      count % 2 ? (
+        <div className="row ministry ministry-left" key={ministry.id}>
+          <div className="image">
+            <Img fluid={ministry.image.childImageSharp.fluid} />
+          </div>
+          <div className="text">
+            <h3>{ministry.title}</h3>
+            <ReactMarkdown source={ministry.description} />
+          </div>
+        </div>
+      ) : (
+        <div className="row ministry ministry-right dark" key={ministry.id}>
+          <div className="text">
+            <h3>{ministry.title}</h3>
+            <div className="description">
+              <ReactMarkdown source={ministry.description} />
+            </div>
+          </div>
+          <div className="image">
+            <Img fluid={ministry.image.childImageSharp.fluid} />
+          </div>
+        </div>
+      )
+    count++
+    return html
+  })
+
+  console.log(renderHtml)
+
   return (
     <Layout>
       <SEO title="Church" />
@@ -19,19 +53,7 @@ const IndexPage = ({ data }) => {
         text={data.allStrapiChurch.nodes[0].bannertext}
         markdown={true}
       />
-      <div className="church">
-        {data.allStrapiChurch.nodes[0].ministry.map(ministry => (
-          <div className="row" key={ministry.id}>
-            <div className="text">
-              <h3>{ministry.title}</h3>
-              <div className="description">{ministry.description}</div>
-            </div>
-            <div className="image">
-              <Img fluid={ministry.image.childImageSharp.fluid} />
-            </div>
-          </div>
-        ))}
-      </div>
+      <div className="church">{renderHtml}</div>
     </Layout>
   )
 }
